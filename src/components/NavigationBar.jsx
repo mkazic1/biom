54bad34ae192db91c2da63ed1f1413b69be53335
Medina <mkazic1@etf.unsa.ba>
2023-04-15 16:54:29 +0200

    Navigation bar and style implemented

---
+++ src/components/NavigationBar.jsx
@@ -0,0 +1,77 @@
+import {
+  AppBar,
+  Toolbar,
+  Container,
+  Typography,
+  CardMedia,
+  Box,
+} from '@mui/material';
+import Logo from '../assets/logo.jpg';
+import Cart from '../assets/cart2.png';
+import {
+  appBarStyle,
+  leftContainerStyle,
+  rightContainerStyle,
+  containersStyle,
+  navigationTitleStyle,
+  logoStyle,
+  cartStyle,
+} from '../style/components/NavigationBarStyle';
+
+const NavigationBar = () => (
+  <AppBar position="fixed" sx={appBarStyle}>
+    <Box sx={containersStyle}>
+      <Container maxWidth="false" style={leftContainerStyle}>
+        <Box>
+          <Toolbar variant="dense" style={{ paddinLeft: '0px' }}>
+            <Typography
+              sx={navigationTitleStyle}
+            >
+              SHOP
+            </Typography>
+            <Typography
+              sx={navigationTitleStyle}
+            >
+              WHY BIOM
+            </Typography>
+            <Typography
+              sx={navigationTitleStyle}
+            >
+              SCENTS
+            </Typography>
+          </Toolbar>
+        </Box>
+      </Container>
+      <CardMedia
+        image={Logo}
+        alt="LogoImage"
+        component="img"
+        sx={logoStyle}
+      />
+      <Container maxWidth="false" style={rightContainerStyle}>
+        <Box>
+          <Toolbar variant="dense">
+            <Typography
+              sx={navigationTitleStyle}
+            >
+              SIGN IN
+            </Typography>
+            <Typography
+              sx={{ ...navigationTitleStyle, paddingRight: '12px' }}
+            >
+              CART
+            </Typography>
+            <CardMedia
+              image={Cart}
+              alt="CartImage"
+              component="img"
+              sx={cartStyle}
+            />
+          </Toolbar>
+        </Box>
+      </Container>
+    </Box>
+  </AppBar>
+);
+
+export default NavigationBar;
---
+++ src/style/components/NavigationBarStyle.js
@@ -0,0 +1,45 @@
+export const appBarStyle = {
+  height: '68px',
+  bgcolor: 'secondary.light',
+  boxShadow: 'none',
+};
+
+export const containersStyle = {
+  display: 'flex',
+  justifyContent: 'space-between',
+};
+
+export const leftContainerStyle = {
+  display: 'flex',
+  justifyContent: 'start',
+  paddingLeft: '26px',
+};
+
+export const rightContainerStyle = {
+  display: 'flex',
+  justifyContent: 'end',
+  paddingRight: '50px',
+};
+
+export const navigationTitleStyle = {
+  color: 'text.third',
+  fontWeight: 'bold',
+  letterSpacing: '1.5px',
+  paddingRight: '40px',
+  paddingTop: '23px',
+  paddingBottom: '23px',
+};
+
+export const logoStyle = {
+  width: '92px',
+  height: '30px',
+  paddingTop: '19px',
+  paddingBottom: '19px',
+};
+
+export const cartStyle = {
+  width: '35px',
+  height: '35px',
+  paddingTop: '19px',
+  paddingBottom: '19px',
+};
