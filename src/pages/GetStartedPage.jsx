ecb35c62679ea93e4a940770188a44d9b01b1800
Medina <mkazic1@etf.unsa.ba>
2023-04-16 12:06:58 +0200

    Fixed bug with x overflow, problem was because borderSizing wasnt added

--- src/pages/GetStartedPage.jsx
+++ src/pages/GetStartedPage.jsx
@@ -1,26 +1,33 @@
 import {
-  Typography, Box, Button,
+  Typography,
+  Box,
+  Button,
 } from '@mui/material';
 import backgroundImg from '../assets/Kitchen_3.jpg';
-import { backgroundStyle, contentStyle } from '../style/pages/GetStartedPageStyle';
+import {
+  backgroundStyle,
+  containerStyle,
+  contentStyle,
+  textAndButtonStyle,
+  titleStyle,
+} from '../style/pages/GetStartedPageStyle';
 
 const GetStartedPage = () => (
-  <Box sx={{ paddingTop: '68px', paddingX: '20px', width: '100%' }}>
+  <Box sx={containerStyle}>
     <Box style={{ ...backgroundStyle, backgroundImage: `url(${backgroundImg})` }}>
       <Box sx={contentStyle}>
-        <Typography component="h2" variant="h2" sx={{ color: 'text.secondary' }}>So fresh.</Typography>
-        <Typography component="h2" variant="h2" sx={{ color: 'text.secondary' }}>So green.</Typography>
-        <Typography
-          variant="h6"
-          sx={{
-            color: 'text.secondary', marginTop: '14px', width: '460px', height: '113px',
-          }}
-        >
-          We believe that a more eco-friendly everyday makes a happier you and me.
-          We’re on a mission to put sustainability in reach with better-for-the-planet wipes that are easy, effective,
-          and plastic-free.
-        </Typography>
-        <Button variant="outlined" sx={{ marginTop: '38px' }}>GET STARTED</Button>
+        <Box sx={titleStyle}>
+          <Typography component="h2" variant="h2">So fresh.</Typography>
+          <Typography component="h2" variant="h2">So green.</Typography>
+        </Box>
+        <Box sx={textAndButtonStyle}>
+          <Typography variant="h6">
+            We believe that a more eco-friendly everyday makes a happier you and me.
+            We’re on a mission to put sustainability in reach with better-for-the-planet wipes that are easy, effective,
+            and plastic-free.
+          </Typography>
+          <Button variant="outlined" sx={{ marginTop: '24px' }}>GET STARTED</Button>
+        </Box>
       </Box>
     </Box>
   </Box>
