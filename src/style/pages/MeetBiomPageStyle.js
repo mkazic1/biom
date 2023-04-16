1c1d2bdc0d723586bfb64c8c2e48e4364f4b163b
Medina <mkazic1@etf.unsa.ba>
2023-04-16 17:27:25 +0200

    Meet biom page and styling implemented, missing vertical scroll on meet biom

---
+++ src/style/pages/MeetBiomPageStyle.js
@@ -0,0 +1,41 @@
+export const backgroundStyle = {
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat',
+  height: '100vh',
+  width: '100%',
+};
+
+export const containerStyle = {
+  width: '100%',
+  boxSizing: 'border-box',
+};
+
+export const contentStyle = {
+  paddingTop: '103px',
+  paddingLeft: '735px',
+  paddingRight: '44px',
+};
+
+export const textAndButtonStyle = {
+  color: 'text.primary',
+  marginTop: '14px',
+  width: '460px',
+  height: '220px',
+};
+
+export const titleStyle = {
+  width: '100%',
+  height: '150px',
+  color: 'text.primary',
+};
+
+export const iconStyle = {
+  width: '69px',
+  height: '67px',
+};
+
+export const iconsTextBoxStyle = {
+  display: 'flex',
+  justifyContent: 'start',
+  alignItems: 'center',
+};
