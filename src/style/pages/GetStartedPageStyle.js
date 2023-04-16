eab94e309dc3a694824b7e27c25acf5bd78774ef
Medina <mkazic1@etf.unsa.ba>
2023-04-16 12:07:14 +0200

    Fixed bug with x overflow, problem was because borderSizing wasnt added

--- src/style/pages/GetStartedPageStyle.js
+++ src/style/pages/GetStartedPageStyle.js
@@ -1,11 +1,32 @@
 export const backgroundStyle = {
   backgroundSize: 'cover',
-  width: '100%',
+  backgroundRepeat: 'no-repeat',
   height: '100vh',
+  width: '100%',
   borderRadius: '14px',
+};
+
+export const containerStyle = {
+  paddingTop: '68px',
+  paddingX: '14px',
+  width: '100%',
+  boxSizing: 'border-box',
 };
 
 export const contentStyle = {
   paddingTop: '190px',
   paddingLeft: '85px',
+};
+
+export const textAndButtonStyle = {
+  color: 'text.secondary',
+  marginTop: '14px',
+  width: '460px',
+  height: '113px',
+};
+
+export const titleStyle = {
+  width: '270px',
+  height: '150px',
+  color: 'text.secondary',
 };
