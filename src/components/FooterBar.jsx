ad91a24d040af70a23e2782388bd91b79e8bcbaa
Medina <mkazic1@etf.unsa.ba>
2023-04-17 17:42:29 +0200

    Implemented bottom footer bar and styling

---
+++ src/components/FooterBar.jsx
@@ -0,0 +1,98 @@
+import {
+  Toolbar,
+  Typography,
+  CardMedia,
+  Box,
+} from '@mui/material';
+import Logo from '../assets/logo.png';
+import Instagram from '../assets/Icon ionic-logo-instagram.png';
+import Facebook from '../assets/Icon awesome-facebook-f.png';
+import {
+  containersStyle,
+  navigationTitleStyle,
+  logoStyle,
+  bottomContainerStyle,
+  instagramIcon,
+  facebookIcon,
+  iconBackgroundStyle,
+} from '../style/components/FooterBarStyle';
+
+const FooterBar = () => (
+  <Box sx={{ marginTop: '50px' }}>
+    <Box sx={containersStyle}>
+      <Box>
+        <Toolbar variant="dense" style={{ paddingLeft: '0px' }}>
+          <Typography
+            sx={navigationTitleStyle}
+          >
+            SHOP
+          </Typography>
+          <Typography
+            sx={navigationTitleStyle}
+          >
+            WHY BIOM
+          </Typography>
+          <Typography
+            sx={{ ...navigationTitleStyle, paddingRight: '79px' }}
+          >
+            SCENTS
+          </Typography>
+        </Toolbar>
+      </Box>
+      <CardMedia
+        image={Logo}
+        alt="LogoImage"
+        component="img"
+        sx={logoStyle}
+      />
+      <Box>
+        <Toolbar variant="dense" style={{ paddingRight: '0px' }}>
+          <Typography
+            sx={{ ...navigationTitleStyle, paddingLeft: '75px' }}
+          >
+            FAQS
+          </Typography>
+          <Typography
+            sx={navigationTitleStyle}
+          >
+            ACCOUNT
+          </Typography>
+          <Typography
+            sx={navigationTitleStyle}
+          >
+            HELP
+          </Typography>
+        </Toolbar>
+      </Box>
+    </Box>
+    <Box sx={bottomContainerStyle}>
+      <Box>
+        <Toolbar variant="dense" style={{ paddingLeft: '0px' }}>
+          <Typography sx={{ color: 'secondary.main', letterSpacing: '0.40px' }}>
+            © 2021 biom. All rights reserved • Privacy Policy • Terms of Service
+          </Typography>
+        </Toolbar>
+      </Box>
+      <Box sx={{ display: 'flex', justifyConten: 'space-between' }}>
+        <Box sx={{ ...iconBackgroundStyle, marginRight: '15px' }}>
+          <CardMedia
+            image={Instagram}
+            alt="Instagram"
+            component="img"
+            sx={instagramIcon}
+          />
+        </Box>
+        <Box sx={iconBackgroundStyle}>
+          <CardMedia
+            image={Facebook}
+            alt="Instagram"
+            component="img"
+            sx={facebookIcon}
+          />
+        </Box>
+      </Box>
+    </Box>
+  </Box>
+);
+
+export default FooterBar;
