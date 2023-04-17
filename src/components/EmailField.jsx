3ad3a3ff00acee2e2394d5db726ab758c78b7788
Medina <mkazic1@etf.unsa.ba>
2023-04-17 17:41:41 +0200

    Implemented email field and styling, needed for footer

---
+++ src/components/EmailField.jsx
@@ -0,0 +1,24 @@
+import { Box, CardMedia, Typography } from '@mui/material';
+import arrowIcon from '../assets/Icon ionic-ios-arrow-round-forward-1.png';
+import {
+  containerStyle,
+  iconBackgroundStyle,
+  iconStyle,
+  textStyle,
+} from '../style/components/EmailFieldStyle';
+
+const EmailField = () => (
+  <Box sx={containerStyle}>
+    <Typography sx={textStyle}>Enter your email address</Typography>
+    <Box sx={iconBackgroundStyle}>
+      <CardMedia
+        image={arrowIcon}
+        alt="DurableImage"
+        component="img"
+        sx={iconStyle}
+      />
+    </Box>
+  </Box>
+);
+
+export default EmailField;
---
+++ src/style/components/EmailFieldStyle.js
@@ -0,0 +1,33 @@
+export const containerStyle = {
+  width: '440px',
+  display: 'flex',
+  justifyContent: 'space-between',
+  backgroundColor: 'secondary.light',
+  padding: '10px',
+  marginTop: '18px',
+  borderRadius: '6px',
+};
+
+export const iconStyle = {
+  width: '18px',
+  height: '12px',
+};
+
+export const iconBackgroundStyle = {
+  width: '44px',
+  height: '38px',
+  backgroundColor: 'primary.light',
+  borderRadius: '6px',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  '&:hover': {
+    backgroundColor: 'primary.main',
+  },
+};
+
+export const textStyle = {
+  display: 'flex',
+  alignItems: 'center',
+  flexWrap: 'nowrap',
+};
