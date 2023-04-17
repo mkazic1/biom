527dc9a10de2b50d266d4f305e1d61984064d778
Medina <mkazic1@etf.unsa.ba>
2023-04-17 17:43:20 +0200

    Footer implementation completed, finally

---
+++ src/style/components/FooterStyle.js
@@ -0,0 +1,36 @@
+export const imageBoxStyle = {
+  borderRadius: '14px',
+  backgroundColor: 'secondary.dark',
+};
+
+export const backgroundStyle = {
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat',
+  height: '300px',
+  width: '100%',
+};
+
+export const containerStyle = {
+  paddingTop: '50px',
+  paddingX: '69px',
+  height: '100%',
+  width: '100%',
+  boxSizing: 'border-box',
+  backgroundImage: 'linear-gradient(to bottom, #FFFFFF 30%, #487D7F 30%, #487D7F 100% )',
+};
+
+export const titleStyle = {
+  width: '370px',
+  color: 'text.third',
+};
+
+export const descriptionStyle = {
+  width: '440px',
+  color: 'text.third',
+  marginTop: '13px',
+};
+
+export const contentStyle = {
+  paddingTop: '47px',
+  paddingLeft: '111px',
+};
