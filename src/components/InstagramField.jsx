382a666561f9c90b760185b70ec356ebb71a2eec
Medina <mkazic1@etf.unsa.ba>
2023-04-18 02:36:18 +0200

    Implemented insta field/button

---
+++ src/components/InstagramField.jsx
@@ -0,0 +1,25 @@
+import { Box, CardMedia, Typography } from '@mui/material';
+import instagramIcon from '../assets/Icon awesome-instagram.png';
+import {
+  containerStyle,
+  iconBackgroundStyle,
+  iconStyle,
+  textStyle,
+} from '../style/components/InstagramFieldStyle';
+
+const InstagramField = () => (
+  <Box sx={containerStyle}>
+    <Typography sx={textStyle}>FOLLOW US ON INSTAGRAM</Typography>
+    <Box sx={{ borderLeft: 'solid 1px #002D33' }} />
+    <Box sx={iconBackgroundStyle}>
+      <CardMedia
+        image={instagramIcon}
+        alt="InstagramIcon"
+        component="img"
+        sx={iconStyle}
+      />
+    </Box>
+  </Box>
+);
+
+export default InstagramField;
---
+++ src/style/components/InstagramFieldStyle.js
@@ -0,0 +1,37 @@
+export const containerStyle = {
+  width: '330px',
+  display: 'flex',
+  justifyContent: 'space-between',
+  backgroundColor: 'secondary.light',
+  color: 'text.primary',
+  marginTop: '18px',
+  borderRadius: '6px',
+  border: 'solid 1px #002D33',
+  paddingLeft: '15px',
+};
+
+export const iconStyle = {
+  width: '18px',
+  height: '18px',
+};
+
+export const iconBackgroundStyle = {
+  width: '44px',
+  height: '38px',
+  backgroundColor: 'secondary.light',
+  border: 'none',
+  margin: '0.3px',
+  padding: '10px',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  '&:hover': {
+    backgroundColor: 'primary.main',
+  },
+};
+
+export const textStyle = {
+  display: 'flex',
+  alignItems: 'center',
+  fontWeight: 'bold',
+};
