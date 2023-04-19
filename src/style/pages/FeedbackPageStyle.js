5cd3c25909344246652c922f31f96bc179b11dae
Medina <mkazic1@etf.unsa.ba>
2023-04-19 18:27:28 +0200

    Feedback page implemented and styled

---
+++ src/style/pages/FeedbackPageStyle.js
@@ -0,0 +1,35 @@
+export const containerStyle = {
+  width: '100%',
+  backgroundColor: 'secondary.dark',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  flexDirection: 'column',
+  color: 'text.primary',
+  paddingY: '90px',
+};
+
+export const descriptionStyle = {
+  width: '950px',
+  color: 'text.primary',
+  marginY: '26px',
+  textAlign: 'center',
+  fontWeight: 'bold',
+};
+
+export const dotsContainerStyle = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  marginTop: '50px',
+  paddingLeft: '15px',
+};
+
+export const dotsStyle = {
+  width: '10px',
+  height: '10px',
+  borderRadius: '50%',
+  bgcolor: 'primary.main',
+  marginRight: '12px',
+  border: '1px solid',
+};
