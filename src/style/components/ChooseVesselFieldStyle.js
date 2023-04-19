b8a22ef0546b9f9c36b60ef70f24f9b0c76f329e
Medina <mkazic1@etf.unsa.ba>
2023-04-19 23:53:04 +0200

    Implemented choose vessel as separate component

---
+++ src/style/components/ChooseVesselFieldStyle.js
@@ -0,0 +1,35 @@
+export const containerStyle = {
+  width: '360px',
+  display: 'flex',
+  justifyContent: 'space-between',
+  backgroundColor: 'secondary.light',
+  color: 'text.primary',
+  paddingLeft: '15px',
+  borderRadius: '6px',
+  border: 'solid 1px #002D33',
+};
+
+export const iconStyle = {
+  width: '19px',
+  height: '13px',
+};
+
+export const iconBackgroundStyle = {
+  width: '40px',
+  height: '40px',
+  backgroundColor: 'secondary.light',
+  borderRadius: '0px 6px 6px 0px',
+  borderLeft: 'solid 1px #002D33',
+  padding: '10px',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+};
+
+export const textStyle = {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  fontWeight: 'bold',
+  letterSpacing: '1.2px',
+};
