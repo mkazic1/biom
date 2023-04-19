2d5105daec605e37bd3fd0a5d4275e69e1b4c7ba
Medina <mkazic1@etf.unsa.ba>
2023-04-19 23:53:40 +0200

    Implemented pictures display through gallery

---
+++ src/style/components/VesselHorizontalGalleryStyle.js
@@ -0,0 +1,20 @@
+export const containerStyle = {
+  height: '390px',
+  width: '100%',
+  overflow: 'hidden',
+};
+
+export const galleryStyle = {
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-around',
+  width: '100%',
+  '::-webkit-scrollbar': {
+    display: 'none',
+  },
+};
+
+export const imageStyle = {
+  width: '400px',
+  height: '400px',
+};
